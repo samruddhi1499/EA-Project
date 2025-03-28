35ccbdfe53ba184e66bb4133bf8708595298a2df
samruddhi1499 <71025595+samruddhi1499@users.noreply.github.com>
2025-03-28 19:43:35 -0400

    Committing Order service and Eureka Server

---
+++ OrderService-PW/src/main/java/com/service/order/Order.java
@@ -0,0 +1,78 @@
+package com.service.order;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+
+@Document(collection = "orders")
+public class Order {
+    @Id
+    private String id;
+    private String userId;
+    private String stockName;
+    private int quantity;
+    private double price;
+    private String status;
+    
+    private User user;
+    
+    
+	public Order() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Order(String id, String userId, String stockName, int quantity, double price, String status, User user) {
+		super();
+		this.id = id;
+		this.userId = userId;
+		this.stockName = stockName;
+		this.quantity = quantity;
+		this.price = price;
+		this.status = status;
+		this.user = user;	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getStockName() {
+		return stockName;
+	}
+	public void setStockName(String stockName) {
+		this.stockName = stockName;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+    
+    
+    
+}
---
+++ OrderService-PW/src/main/java/com/service/order/OrderService.java
@@ -0,0 +1,30 @@
+package com.service.order;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public Mono<Order> placeOrder(Order order) {
+        order.setStatus("PLACED");
+        return orderRepository.save(order);
+    }
+
+    public Flux<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    public Mono<Order> getOrderById(String orderId) {
+        return orderRepository.findById(orderId);
+    }
+
+    public Flux<Order> getOrdersByUserId(String userId) {
+        return orderRepository.findByUserId(userId);
+    }
+}
