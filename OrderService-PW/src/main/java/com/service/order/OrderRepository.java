36b6cd91401207ed43aebda04ff3b144db88c5d0
SruthiPandiath <144498291+SP393@users.noreply.github.com>
2025-03-29 11:36:32 -0400

    Delete OrderService-PW directory

--- OrderService-PW/src/main/java/com/service/order/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.service.order;
-
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import reactor.core.publisher.Flux;
-
-public interface OrderRepository extends ReactiveMongoRepository<Order, String> {
-    Flux<Order> findByUserId(String userId);
-}
